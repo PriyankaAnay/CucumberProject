e14c80e6a1f02daf374da672b929de9930337c9c
PriyankaAnay -Git <priyankashinde2970@gmail.com>
2022-12-09 16:48:10 +0530

    Priyanka First Commit

---
+++ src/test/java/com/stepDefinition/LoginSteps.java
@@ -0,0 +1,49 @@
+package com.stepDefinition;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginSteps {
+	WebDriver driver;
+
+	@Given("User is on Loginpage")
+	public void user_is_on_loginpage() {
+		System.out.println("Step1: User is on Login page");
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+	}
+
+	@When("user enters username and password")
+	public void user_enters_username_and_password() {
+		System.out.println("Step2: User Enters Username and Password");
+		driver.findElement(By.xpath("//input[@name=\"username\"]")).sendKeys("Admin");
+		driver.findElement(By.xpath("//input[@type=\"password\"]")).sendKeys("admin123");
+	}
+
+	@And("Click on Login button")
+	public void click_on_login_button() {
+		System.out.println("Step3: User is clicking on Login Button");
+		driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button")).click();
+	}
+
+	@Then("user should be land on homepage")
+	public void user_should_be_land_on_homepage() {
+		System.out.println("Step4: User lands on home page");
+	}
+
+}
---
+++ src/test/resources/Features/LoginFeature.feature
@@ -0,0 +1,21 @@
+Feature: Test the login functionality of OrangeHRM
+
+Scenario: Test the valid Login
+
+Given User is on Loginpage
+
+When user enters username and password
+
+And Click on Login button
+
+Then user should be land on homepage
+
+Scenario: Test the valid Login1
+
+Given User is on Loginpage
+
+When user enters username and password
+
+And Click on Login button
+
+Then user should be land on homepage
